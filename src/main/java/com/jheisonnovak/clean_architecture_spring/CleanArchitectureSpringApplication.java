ef28bbd286261db0f6c7b7212515837dde8d38b7
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2025-04-25 11:44:55 -0300

    feat: initialize project structure

---
+++ src/main/java/com/jheisonnovak/clean_architecture_spring/CleanArchitectureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jheisonnovak.clean_architecture_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jheisonnovak/clean_architecture_spring/CleanArchitectureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jheisonnovak.clean_architecture_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
